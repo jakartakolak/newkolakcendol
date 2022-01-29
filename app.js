219969ae323eb529d60fb09d9ac26a1a6ed1e552
khaledyurino <71624020+khaledyurino@users.noreply.github.com>
2022-01-29 16:59:17 +0700

    add env

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
+const port = process.env.PORT || 3000
 const productRoutes = require('./routes/product')
 const userRoutes = require("./routes/user")
 

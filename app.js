4540ddc3668cfe35576a6b6a3fec533eba770958
khaledyurino <71624020+khaledyurino@users.noreply.github.com>
2022-01-29 17:59:35 +0700

    config

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
+const timeout = require('connect-timeout');
 const PORT = process.env.PORT || 3000
 const productRoutes = require('./routes/product')
 const userRoutes = require("./routes/user")
 
-app.set('view engine', 'ejs')
 app.use(express.json());
 
+app.use(timeout('5s'))
 app.use('/',userRoutes)
 app.use('/me',productRoutes)
 

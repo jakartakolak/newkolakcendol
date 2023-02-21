5f2fa286e3afb48749bb3a9b4d5116567f3073b8
khaledyurino.triova <khaledyurino.triova@homecredit.co.id>
2023-02-21 14:21:42 +0700

    Add config database

--- app.js
+++ app.js
@@ -4,11 +4,6 @@ const timeout = require('connect-timeout');
 const PORT = process.env.PORT || 4000
 const productRoutes = require('./routes/product')
 const userRoutes = require("./routes/user")
-require('dotenv').config()
-const mysql = require('mysql2')
-const connection = mysql.createConnection(process.env.DATABASE_URL)
-console.log('Connected to PlanetScale!')
-connection.end()
 
 app.use(express.json());
 
